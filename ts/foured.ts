113c5928af5ac0b45845dcda24d23ce21b9e3b5b
misterscruffles <misterscruffles@gmail.coim>
2020-11-18 22:32:53 -0800

    Added in initiative calculations

--- ts/foured.ts
+++ ts/foured.ts
@@ -1,15 +1,20 @@
-import { PlayerSheet, PlayerActor } from "./actors/player.js";
+import { PlayerActor, PlayerSheet } from "./actors/player.js";
 import { ItemType } from "./item/data.js";
 import { PlayerClassSheet } from "./item/playerClass.js";
 import { WeaponSheet } from "./item/weapon.js";
 
 console.log("foured | Loaded foured.js file");
 
+// Configure standard formulas
+CONFIG.Combat.initiative.formula = "1d20 + @initiative.modifier";
+
 Hooks.once('init', async function () {
     console.log("foured | Starting foured initialization");
 
+    CONFIG.Actor.entityClass = PlayerActor;
+
     Actors.unregisterSheet("core", ActorSheet);
-    Actors.registerSheet("foured", PlayerSheet, {makeDefault: true})
+    Actors.registerSheet("foured", PlayerSheet, {makeDefault: true, types: ["character"], label: "FOURED.Player"})
 
     Items.registerSheet("foured", WeaponSheet, { types: [ItemType.Weapon], makeDefault: true });
     Items.registerSheet("foured", PlayerClassSheet, { type: [ItemType.PlayerClass], makeDefault: true });
